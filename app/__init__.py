9696601b9b4be93c244f6185cb9598293aa0cbb6
SYX-LAN <1196081136@qq.com>
2020-07-06 21:28:38 +0800

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.debug = True
 
-from app import routes
+# db对象表示数据库, 数据库应用和迁移是一个实例
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# models用来定义数据库结构
+from app import routes,models
